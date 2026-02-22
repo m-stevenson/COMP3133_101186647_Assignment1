c8f7c9d62c883639bb17b3268239deff7c07e5af
m-stevenson <mitchell.stevenson@georgebrown.ca>
2026-02-22 18:27:11 -0500

    Added model definitions

--- models/User.js
+++ models/User.js
@@ -1,5 +1,48 @@
 import mongoose from "mongoose";
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    
-})
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+    select: false,
+  },
+  timestamps: {
+    created_at: "created_at",
+    updated_at: "updated_at",
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
